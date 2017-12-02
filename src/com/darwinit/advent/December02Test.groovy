cefa095a68cadafcce19d964b392974777d53f82
Christof Hullaert <christof.hullaert@cover3d.com>
2017-12-02 12:39:36 +0100

    add test for part 2

--- src/com/darwinit/advent/December02Test.groovy
+++ src/com/darwinit/advent/December02Test.groovy
@@ -2,34 +2,33 @@ package com.darwinit.advent
 
 import static org.junit.jupiter.api.Assertions.*
 
+import org.junit.Before
 import org.junit.jupiter.api.Test
 
-class SpreadsheetTest {
-
+class December02Test {
+	
+	def sheet=new Spreadsheet();
+	
 	@Test
 	void GivenOneLineExpectedHaveTheDifferenceOfExtremas() {
-		Spreadsheet sheet=new Spreadsheet();
 		sheet.add("5 1 9 5");
 		assertEquals(8, sheet.compute());
 	}
 
 	@Test
 	void GivenOneLine753ExpectedHaveTheDifferenceOfExtremas4() {
-		Spreadsheet sheet=new Spreadsheet();
 		sheet.add("7    5    3");
 		assertEquals(4, sheet.compute());
 	}
 
 	@Test
 	void GivenOneLine2468ExpectedHaveTheDifferenceOfExtremas6() {
-		Spreadsheet sheet=new Spreadsheet();
 		sheet.add("2    4    6 8");
 		assertEquals(6, sheet.compute());
 	}
 
 	@Test
 	void GivenThreeLines_5195_753_2468ExpectedHaveTheSumofDifferenceOfExtremas18() {
-		Spreadsheet sheet=new Spreadsheet();
 		sheet.add("5 1 9 5");
 		sheet.add("7    5    3");
 		sheet.add("2    4    6 8");
---
+++ src/com/darwinit/advent/December02_Part2Test.groovy
@@ -0,0 +1,14 @@
+package com.darwinit.advent
+
+import static org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class December02_Part2Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented")
+	}
+
+}
